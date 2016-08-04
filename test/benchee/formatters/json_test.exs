5e799f5155ff122459d8d1ab71595ce8a59f237a
Tobias Pfeiffer <pragtob@gmail.com>
2016-08-04 13:40:36 +0200

    Itest that it does not alter the suite

--- test/benchee/formatters/json_test.exs
+++ test/benchee/formatters/json_test.exs
@@ -1,4 +1,26 @@
 defmodule Benchee.Formatters.JSONTest do
   use ExUnit.Case
   doctest Benchee.Formatters.JSON
+
+  @filename "test.json"
+  test ".output returns the suite again unchanged" do
+  suite = %{
+    config: %{
+      json: %{file: @filename}
+      },
+    statistics: %{
+      "My Job" => %{
+        some: "value"
+      }
+    },
+    run_times: %{"My Job" => [1, 2, 3]}
+  }
+
+  try do
+    return = Benchee.Formatters.JSON.output(suite)
+    assert return == suite
+  after
+    File.rm! @filename
+  end
+end
 end
