5b553a639cf7113d2a9bb13e1b5a0f984fd9b9b3
Devon Estes <devon.c.estes@gmail.com>
2018-12-30 13:12:21 +0100

    Add memory usage statistics (#11)
    * Add memory usage statistics
    
    Now our JSON formatter will give us memory usage statistics as well as
    run time statistics.
    
    This is a breaking change, though, so we should only release this as
    1.0. I don't think it will make a whole lot of difference to do this as
    a non-breaking change first. If folks want memory usage statistics, they
    can upgrade to 1.0 (which they really should do anyway).
    
    * Change structure for output JSON
    
    Now the JSON we output has the same structure as our scenarios list -
    just with only the important stuff in the output.

--- test/benchee/formatters/json_integration_test.exs
+++ test/benchee/formatters/json_integration_test.exs
@@ -6,6 +6,7 @@ defmodule Benchee.Formatters.JSONIntegrationTest do
   test "works just fine" do
     basic_test(
       time: 0.01,
+      memory_time: 0.01,
       warmup: 0.02,
       formatters: [{Benchee.Formatters.JSON, file: @file_path}]
     )
@@ -16,22 +17,28 @@ defmodule Benchee.Formatters.JSONIntegrationTest do
       Benchee.run(
         %{
           "Sleep" => fn -> :timer.sleep(10) end,
-          "Sleep longer" => fn -> :timer.sleep(20) end
+          "List" => fn -> [:rand.uniform()] end
         },
         options
       )
 
       assert File.exists?(@file_path)
       content = File.read!(@file_path)
-      decoded = Jason.decode!(content)
+      [list, sleep] = Jason.decode!(content)
 
-      assert %{"run_times" => run_times, "statistics" => statistics} = decoded
-      assert map_size(run_times) == 2
-      assert map_size(statistics) == 2
+      assert %{
+               "name" => "Sleep",
+               "run_time_statistics" => %{"average" => _, "ips" => _},
+               "run_times" => [run_time | _]
+             } = sleep
 
-      assert %{"average" => _average, "ips" => _ips} = statistics["Sleep"]
-      assert [head | _tail] = run_times["Sleep longer"]
-      assert head >= 0
+      assert run_time > 10_000_000
+
+      assert %{
+               "name" => "List",
+               "memory_usage_statistics" => %{"average" => _, "ips" => _},
+               "memory_usages" => [376 | _]
+             } = list
     end)
   after
     File.rm(@file_path)
